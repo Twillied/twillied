5d7005dc009f55f582cea8ee65bbfc4faca3ce94
Twillied <tim@earfood.net>
2019-08-09 07:14:45 -0500

    terraform testing

---
+++ vars.tf
@@ -0,0 +1,11 @@
+variable "site_domain" {
+  default = "bookmyfacetube.com"
+}
+
+variable "bucket_index_document" {
+  default = "index.html"
+}
+
+variable "bucket_error_document" {
+  default = "error.html"
+}
